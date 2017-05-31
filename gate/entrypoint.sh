f520fcac8b0821e40b3645eee2f0504ec0fa1f48
Simon Meoni <smeoni@arges.atilf.fr>
2017-05-31 17:58:48 +0200

    fix private url base & fix entrypoint bash script

--- gate/entrypoint.sh
+++ gate/entrypoint.sh
@@ -3,9 +3,16 @@ while [[ `mysql --user=root --password=$PASSWORD --host=$SQLHOST -e "SHOW DATABA
 do
   sleep 6 | echo "wait db ..."
 done
+
+if [ ! -f /opt/gate-code/build ]
+then
 sed -i s/$\{PASSWORD\}/$PASSWORD/g /opt/gate-code/build.properties
 sed -i s/$\{SQLHOST\}/$SQLHOST/g /opt/gate-code/build.properties
 sed -i s/$\{HOST\}/$HOST/g /opt/gate-code/build.properties
 cd /opt/gate-code/ && ant install
+touch /opt/gate-code/build
+fi
+
+cd /opt/gate-code/
 ./tomcat6/bin/catalina.sh start
 tail -F /opt/gate-code/tomcat6/logs/*
